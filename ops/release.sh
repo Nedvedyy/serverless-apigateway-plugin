c33b9e07d73a542240143612545978f46c4a03e6
Nguyen Luan <luannguyen141194@gmail.com>
2017-09-12 16:07:18 +0700

    update: release.sh

--- ops/release.sh
+++ ops/release.sh
@@ -13,6 +13,8 @@ npm install
 npm prune
 git add .
 
+export TRAVIS_REPO_SLUG=GFG/serverless-apigateway-plugin
+export TRAVIS_BUILD_NUMBER=GFG/serverless-apigateway-plugin
 # TODO Fix need a GH_TOKEN for github-change cli to work, make it option in the script
 bash ./node_modules/gfg-cicd-scripts/modules/build.sh
 
